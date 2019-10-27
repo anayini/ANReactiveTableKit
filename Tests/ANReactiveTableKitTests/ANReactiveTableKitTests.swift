85fa8e05f5cc197e44c51da7e8717e6e844b65e5
Arjun Nayini <aanayini@gmail.com>
2019-10-27 09:22:40 -0700

    Remove example XCTest content

--- Tests/ANReactiveTableKitTests/ANReactiveTableKitTests.swift
+++ Tests/ANReactiveTableKitTests/ANReactiveTableKitTests.swift
@@ -2,12 +2,7 @@ import XCTest
 @testable import ANReactiveTableKit
 
 final class ANReactiveTableKitTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ANReactiveTableKit().text, "Hello, World!")
-    }
+    func testExample() {}
 
     static var allTests = [
         ("testExample", testExample),
